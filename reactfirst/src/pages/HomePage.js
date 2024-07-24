99f0494db00ce27173d55d06872b8b7605ab896e
Brady <bdullanty@ewu.edu>
2024-07-24 11:53:26 -0700

    Updated homepage text to be accurate

--- reactfirst/src/pages/HomePage.js
+++ reactfirst/src/pages/HomePage.js
@@ -8,42 +8,39 @@ export const HomePage = () => {
   return (
     <div className="HomePage">
        <ButtonAppBar/>
-      <header className="HomePage-header">
+
+      <body className='HomePage-body'> 
        
-   
-        <h1 className="Homepage-Title">
+        
+      <h1 className="Homepage-Title">
          Welcome to Brady Dullanty's Porfolio Website.
         </h1>
-        
-
-      </header>
-
-      
-      <body className='HomePage-body'> 
-
+        <br/>
       <h4 className="HomePage-Mission-Statement-title">Mission Statement:</h4>
         <h6 className="HomePage-Mission-Statement">
-          I am a current student at Eastern Washington unviversity looking for an ethical company for which I can work as a software developer
-          Intern.
+          I am looking for an ethical company for which I can work as an entry level software developer.
         </h6>
-
-        <h4>Short Summary:</h4>
+        <br/>
+        <h4 className="HomePage-Mission-Statement-title">About Me:</h4>
         
         <h6 className="HomePage-Mission-Statement">
-          I am a current student at Eastern Washington unviversity looking for an ethical company for which I can work as a software developer
-          Intern. more ect.
+          I am a current student with senior standing at Eastern Washington unviversity. My goal is to prosper as a software engineer under the guidance of a company. 
+          <br/><br/>I have taken a multitude of course with a focus on software development, algorithms, networks, and cybersecurity.
+          <br/><br/>I have a good understanding of Linux server hosting as well, stemming from several networking classes and personal projects.<br/><br/>
+           I have a always had a drive to solve problems, and I find myself passionate to learn new tools and skills on my journey of being a Software Developer.
+          <br/><br/>I am an experience worker, having worked over a year in several industries including the Light and Sound, Food, and Medical. From this experience I have refined my personal and deescalation skils.
           
         </h6>
-        <h4>External Sites:</h4>
+        <br/>
+        <h4 className='ExternalSites-title'>External Sites:</h4>
         <br/>
        <GridOfLinkCards/>
        
        
       </body>
       <footer className="HomePage-footer">
-      <h6 className="HomePage-Footer-Statement">
-        Website created by Brady Dullanty using React, hosted using AWS</h6>
-        </footer>
+         <h6 className="HomePage-Footer-Statement"> Website created by Brady Dullanty using React, hosted using AWS</h6>
+      </footer>
 
     </div>
   );
