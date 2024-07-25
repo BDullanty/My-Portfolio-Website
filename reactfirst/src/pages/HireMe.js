2115603585ae2c3a3c8bdc49175826dadbfc3f33
Brady <bdullanty@ewu.edu>
2024-07-25 14:25:10 -0700

    Added in basic HireMe page

--- reactfirst/src/pages/HireMe.js
+++ reactfirst/src/pages/HireMe.js
@@ -1,5 +1,8 @@
 import {ButtonAppBar} from "../Components/AppBar";
+import Paper from '@mui/material/Paper';
 import './HireMe.css';
+import '../Components/LinkCards';
+import { GithubLinkCard, GridOfLinkCards, HackerRankLinkCard } from "../Components/LinkCards";
 
 export const HireMe = () => {
   return (
@@ -7,10 +10,21 @@ export const HireMe = () => {
       <ButtonAppBar/>
       <header className="HireMe-header">
 
+        <Paper elevation={3} />
 
       </header>
       <body className='HireMe-body'>
+        <Paper elevation={20} className="HireMe-Paper-content">
+        <h4 className="HireMe-Paper-titletext">Contact Information</h4>
+          <p className="HireMe-ContentText">Prefered contact is by email.</p>
+          <p className="HireMe-ContentText">Email: Bdullanty@gmail.com</p>
 
+          <p className="HireMe-ContentText">Phone: 775 *** ****</p>
+          <h5 className="HireMe-MediaTitle">SocialMedia:</h5>
+          <br/>
+          <GridOfLinkCards/>
+        </Paper >
+        <Paper elevation={3} />
       </body >
       
       <footer className="HireMe-footer">
