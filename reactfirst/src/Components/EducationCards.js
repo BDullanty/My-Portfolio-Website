57168f2418badc067a6f905bf45aa7f8faf2777f
Brady <bdullanty@ewu.edu>
2024-07-25 15:26:22 -0700

    Added in classes 202-260

--- reactfirst/src/Components/EducationCards.js
+++ reactfirst/src/Components/EducationCards.js
@@ -18,22 +18,22 @@ export function GridOfEducationCards(){
     >
         
         <Grid item>
-        <Class210/>
+        <Class202/>
         </Grid>
         <Grid item>
         <Class210/>
         </Grid>
         <Grid item>
-        <Class210/>
+        <Class211/>
         </Grid>
         <Grid item>
-        <Class210/>
+        <Class212/>
         </Grid>
         <Grid item>
-        <Class210/>
+        <Class240/>
         </Grid>
         <Grid item>
-        <Class210/>
+        <Class260/>
         </Grid>
          <Grid item>
         <Class210/>
@@ -60,6 +60,32 @@ export function GridOfEducationCards(){
 
 
 
+export function Class202() { 
+  return (
+          <Card  className="ClassCard" sx={{ minWidth: 275, boxShadow:3,
+            ':hover': {
+          boxShadow: 20,}}}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+               CSCD202
+              </Typography>
+              <Typography variant="h5" component="div">
+              Computing Ethics
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                A-
+              </Typography>
+              <Typography variant="body2">
+              his course explores the uses of computing technologies from a socio-cultural and ethical perspective, including the impacts of information systems on individuals, organization, and society and future direction in which the forces of technology and computing are tending to move us.
+              
+              </Typography>
+            </CardContent>
+
+          </Card>
+      
+  )
+  
+}
 
 export function Class210() { 
     return (
@@ -74,19 +100,108 @@ export function Class210() {
                 Java Principals
                 </Typography>
                 <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  A+
+                  A
                 </Typography>
                 <Typography variant="body2">
-                  An introduction to java
+                Topics include primitive data types, number systems, file I/O classes, control structures, method design and usage, array–sorting and searching algorithms.
                 
                 </Typography>
               </CardContent>
-              <CardActions>
-                
-                <Button size="small">Learn More</Button>
-              </CardActions>
+
             </Card>
         
     )
     
 }
+export function Class211() { 
+  return (
+          <Card  className="ClassCard" sx={{ minWidth: 275, boxShadow:3,
+            ':hover': {
+          boxShadow: 20,}}}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+               CSCD211
+              </Typography>
+              <Typography variant="h5" component="div">
+              Java Principals II
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                A
+              </Typography>
+              <Typography variant="body2">
+              Topics include composition, recursion, data abstraction, polymorphism, inheritance, interfaces, inner classes, abstract classes, object cloning, array lists, linked lists, and exception handling.
+              </Typography>
+            </CardContent>
+          </Card>
+  )
+}
+
+export function Class212() { 
+  return (
+          <Card  className="ClassCard" sx={{ minWidth: 275, boxShadow:3,
+            ':hover': {
+          boxShadow: 20,}}}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+               CSCD212
+              </Typography>
+              <Typography variant="h5" component="div">
+              Object Oriented Programming
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                B+
+              </Typography>
+              <Typography variant="body2">
+              This course involves a deeper look at object-oriented principles including commonly used design patterns. UML class diagrams, unit testing, and code versioning.
+              </Typography>
+            </CardContent>
+          </Card>
+  )
+}
+
+export function Class240() { 
+  return (
+          <Card  className="ClassCard" sx={{ minWidth: 275, boxShadow:3,
+            ':hover': {
+          boxShadow: 20,}}}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+               CSCD240
+              </Typography>
+              <Typography variant="h5" component="div">
+              C and Unix Programming
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                B-
+              </Typography>
+              <Typography variant="body2">
+              UNIX topics include interactive shells, common text editors, utility programs, file system structure, libraries and operating system calls and system programming. C topics include data types, structures, pointers and pointer arithmetic, arrays, linked lists, and function design and use.             
+              </Typography>
+              </CardContent>
+              
+          </Card>
+  )
+}
+export function Class260() { 
+  return (
+          <Card  className="ClassCard" sx={{ minWidth: 275, boxShadow:3,
+            ':hover': {
+          boxShadow: 20,}}}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+               CSCD260
+              </Typography>
+              <Typography variant="h5" component="div">
+              Architecture And Organization
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                A-
+              </Typography>
+              <Typography variant="body2">
+              This course involves a deeper look at object-oriented principles including commonly used design patterns. UML class diagrams, unit testing, and code versioning.
+              </Typography>
+            </CardContent>
+          </Card>
+  )
+}
+
