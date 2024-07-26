7b81a39fd681910ffe236f5fc3290cb257c703e0
Brady <bdullanty@ewu.edu>
2024-07-25 18:46:21 -0700

    Implemented footer component

---
+++ reactfirst/src/Components/footer.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './footer.css';
+export function PageFooter() {
+
+  return (
+    <footer className="Footer">
+    <h6 className="Footer-Statement">
+      Website created by Brady Dullanty using React, hosted using AWS</h6>
+    </footer>
+
+  );      
+}
