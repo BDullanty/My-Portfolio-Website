80fb24fbdcc4f84c6e48e9fa74b8b2b34a804ed5
Brady <bdullanty@ewu.edu>
2024-07-25 19:26:53 -0700

    Reformatted Home Page

--- reactfirst/src/pages/HomePage.js
+++ reactfirst/src/pages/HomePage.js
@@ -8,45 +8,55 @@ import {Paper} from '@mui/material';
 export const HomePage = () => {
   return (
     <div className="HomePage">
+      
        <ButtonAppBar/>
+      
        <header className='HomePage-header'>
-       <h1 className="Homepage-Title">
-         Welcome to Brady Dullanty's Porfolio Website.
-        </h1>
+       
+          <h1 className="Homepage-Title">
+          Welcome to Brady Dullanty's Porfolio Website.
+          </h1>
        </header>
 
       <body className='HomePage-body'> 
-       
-   
-        
-    
-      <h4 className="HomePage-Mission-Statement-title">Mission Statement:</h4>
-        <h6 className="HomePage-Mission-Statement">
-          I am looking for an ethical company for which I can work as a software developer on the entry/intern level.
-        </h6>
-        <br/>
-       
-        <br/>
-        <Paper elevation={10}>
-        <h4 className="HomePage-Mission-Statement-title">About Me:</h4>
-        
-        <h6 className="HomePage-Mission-Statement">
-          I am a current student with senior standing at Eastern Washington University in Spokane Washington. 
-          <br/><br/>I have taken a multitude of courses with a focus on specific programming languages, software development, algorithms, networks, and cybersecurity.
-          <br/><br/>I have a good understanding of Linux clients and servers, stemming from several networking classes and personal projects.
-          <br/><br/>I have a always had a drive to solve problems, and I find myself passionate to learn new tools and skills on my journey of being a Software Developer.
+      
+        <Paper elevation={20} className='HomepageOuterPaper'>
+            <Paper elevation={1} className='HomepageInnerPaper'>
+                <h4 className="HomePage-Mission-Statement-title">Mission Statement:</h4>
+                <h6 className="HomePage-Mission-Statement">
+                  I am looking for an ethical company for which I can work as a software developer on the entry/intern level.
+                </h6>
+            
+                <br/>
+              
+            </Paper>
+            
+            <Paper elevation={1} className='HomepageInnerPaper'>
+              <h4 className="HomePage-Mission-Statement-title">About Me:</h4>
+              
+              <h6 className="HomePage-Mission-Statement">
+                I am a current student with senior standing at Eastern Washington University in Spokane Washington. 
+                <br/><br/>I have taken a multitude of courses with a focus on specific programming languages, software development, algorithms, networks, and cybersecurity.
+                <br/><br/>I have a good understanding of Linux clients and servers, stemming from several networking classes and personal projects.
+                <br/><br/>I have a always had a drive to solve problems, and I find myself passionate to learn new tools and skills on my journey of being a Software Developer.
+                
+              </h6>
+            </Paper>
+            <Paper elevation={1} className='HomepageInnerPaper-small'>
+                <br/>
+                <h4 className='ExternalSites-title'>External Sites:</h4>
+                <br/>
+                <GridOfLinkCards/>
           
-        </h6>
+          </Paper>
+
+
         </Paper>
         <br/>
-        <h4 className='ExternalSites-title'>External Sites:</h4>
-        <br/>
-       <GridOfLinkCards/>
-       
-       
       </body>
+     
       <PageFooter/>
-
+      
     </div>
   );
 }
