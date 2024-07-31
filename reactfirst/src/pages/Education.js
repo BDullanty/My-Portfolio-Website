8c77633a95857b7f042db2b2058c38061122c898
Brady <bdullanty@ewu.edu>
2024-07-30 17:32:27 -0700

    Added font into coursework page

--- reactfirst/src/pages/Education.js
+++ reactfirst/src/pages/Education.js
@@ -9,9 +9,9 @@ export const Education = () => {
       <ButtonAppBar/>
       
       <header className="Education-header">
-        <h1 className="Education-Mission-Statement-title">Completed Coursework</h1>
+        <h1 className="Education-title">Completed Coursework</h1>
         <br/>
-        <h5 className="Education-Mission-Statement">
+        <h5 className="Education-subtitle">
             I am currently a senior at Eastern Washington University, having completed over twenty-three courses in Computer Science and Mathematics. These classes have given me a solid foundation in software development, programming languages, algorithms, networks, and cybersecurity.
         </h5>
         </header>
