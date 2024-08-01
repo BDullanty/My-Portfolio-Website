2e4d7bafb70d9c9a79f04b546166d1ca4753eca1
Brady <bdullanty@ewu.edu>
2024-07-31 17:25:23 -0700

    Added Fade InDown and fixed card height issue

--- reactfirst/src/Components/Animations.js
+++ reactfirst/src/Components/Animations.js
@@ -1,5 +1,5 @@
 
-import {fadeInRight, fadeInLeft, fadeInUp, fadeIn}from 'react-animations';
+import {fadeInRight, fadeInLeft, fadeInUp, fadeIn,fadeInDown}from 'react-animations';
 import styled, { keyframes } from "styled-components";
 
 export const FadeInRight = styled.div`
@@ -19,14 +19,21 @@ export const FadeInLeft = styled.div`
    `;
 export const FadeInUp = styled.div`
    opacity: 0;
-   animation: 1s ${keyframes`${fadeInUp}`} ;
+   animation: ${props => props.speed || '1s'} ${keyframes`${fadeInUp}`} ;
    animation-delay:  ${props => props.delay || '0s'};
    animation-fill-mode: forwards;
+   height:100%;
   }
    `;
 export const FadeIn = styled.div`
    opacity: 0;
    animation: 2s ${keyframes`${fadeIn}`} ;
+   animation-delay:  ${props => props.delay || '0s'};
+   animation-fill-mode: forwards;
+  }
+   `;export const FadeInDown = styled.div`
+   opacity: 0;
+   animation: ${props => props.speed || '1s'} ${keyframes`${fadeInDown}`} ;
    animation-delay:  ${props => props.delay || '0s'};
    animation-fill-mode: forwards;
   }
