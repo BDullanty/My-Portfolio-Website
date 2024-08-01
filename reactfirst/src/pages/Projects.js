d721fa6daa5e46dfcfcc9e6e066065979f9c269a
Brady <bdullanty@ewu.edu>
2024-07-31 17:08:25 -0700

    Added in animations for Projects.js

--- reactfirst/src/pages/Projects.js
+++ reactfirst/src/pages/Projects.js
@@ -3,16 +3,18 @@ import '../Components/ProjectCards.js';
 import { GridOfProjectCards } from "../Components/ProjectCards.js";
 import './Projects.css';
 import { PageFooter } from "../Components/footer";
+import { FadeInRight } from "../Components/Animations.js";
 export const Projects = () => {
   return (
     <div className="Projects">
       <ButtonAppBar/>
       <header className="Projects-header">
-
+        <FadeInRight>
       <h1 className="Projects-Title">Projects</h1>
+    
       <br/>
       <h5 className="Projects-Secondary-Title">Feel free to click on each to view source code.</h5>
-    
+      </FadeInRight>
       </header>
       <body className='Projects-body'>
         <br/>
