7ccc0a6891e4c9d48a700584336e2a1a16ec0f64
Brady <bdullanty@ewu.edu>
2024-07-31 17:48:46 -0700

    Swapped animation for Education page title

--- reactfirst/src/pages/Education.js
+++ reactfirst/src/pages/Education.js
@@ -3,20 +3,20 @@ import './Education.css';
 import '../Components/EducationCards';
 import { GridOfEducationCards } from "../Components/EducationCards";
 import { PageFooter } from "../Components/footer";
-import {FadeInDown} from "../Components/Animations";
+import {FadeIn} from "../Components/Animations";
 export const Education = () => {
   return (
     <div className="Education">
       <ButtonAppBar/>
       
       <header className="Education-header">
-        <FadeInDown speed='.75s'>
+        <FadeIn speed='4s'>
         <h1 className="Education-title">Completed Coursework</h1>
         <br/>
         <h5 className="Education-subtitle">
             I am currently a senior at Eastern Washington University, having completed over twenty-three courses in Computer Science and Mathematics. These classes have given me a solid foundation in software development, programming languages, algorithms, networks, and cybersecurity.
         </h5>
-        </FadeInDown>
+        </FadeIn>
         </header>
       <body className='Education-body'>
         <GridOfEducationCards/>
