72f2c3244e4b9d6feb6736f5a4bcd475f7e35f5f
Brady Dullanty <bdullanty@ewu.edu>
2024-10-02 13:57:09 -0700

    Changed and improved homepage card wording

--- reactfirst/src/Components/HomePageCards.js
+++ reactfirst/src/Components/HomePageCards.js
@@ -51,7 +51,7 @@ export function HomePageMissionStatement() {
           boxShadow: 15}}}>
            <h4 className="HomePage-Card-Title">Mission Statement:</h4>
            <h6 className="HomePage-Card-Content">
-            I am eager to contribute to an ethical company as an entry-level or intern software developer.
+            I am eager to contribute to an ethical company as an intern or entry-level software developer.
             <br/><br/> I aim to apply my skills and enthusiasm for technology to support impactful projects and advance my professional development.
             <br/><br/> I am committed to making a positive impact and learning from experienced professionals in a collaborative environment.
            </h6>
@@ -65,19 +65,16 @@ export function HomePageAboutMe() {
         <Paper elevation={10} className='HomepageInnerPaper'sx={{background:'var(--card-background-color)', color:'var(--primary-text-color)',
             ':hover': {
           boxShadow: 15}}}>
-        <h4 className="HomePage-Card-Title">About Me:</h4>
+        <h4 className="HomePage-Card-Title">About Brady:</h4>
         <h6 className="HomePage-Card-Content">
-
-        I am a senior at Eastern Washington University in Spokane, Washington, focusing on software development. My academic journey has provided me with a solid foundation in various aspects of software engineering.
-        <br/><br/>
-        Through my coursework, I have gained extensive knowledge in programming languages, which has honed my ability to write clean, efficient, and maintainable code. I have also delved deeply into algorithms, equipping me with the skills to tackle complex problem-solving tasks and optimize performance.
-        <br/><br/>
-        In addition to programming and algorithms, I have studied cybersecurity, which has heightened my awareness of security principles and best practices for developing secure applications. This knowledge is crucial for understanding and mitigating potential vulnerabilities in software.
-        <br/><br/>
-        My experience with Linux clients and servers is both academic and practical. In my coursework, I have engaged in assignments and projects that involve managing and configuring Linux environments. Additionally, personal projects have allowed me to apply and refine my skills in real-world scenarios.
-        <br/><br/>
-        Driven by a passion for software development, I am motivated by the challenge of solving problems and the opportunity to continuously learn and adapt to new tools and technologies. I am excited to leverage my skills and enthusiasm as I advance in my career as a Software Developer.
-        </h6><br/>
+                Brady Dullanty is a dedicated computer science student with six months remaining until graduation in March 2024.
+                <br /><br />
+                He has honed strong skills in program development, cybersecurity, computer networking, and cloud computing, particularly with AWS services.
+                <br /><br />
+                Through diverse coursework in programming, mathematics, cybersecurity, and networking, he has gained the expertise necessary to build robust applications free from vulnerabilities.
+                <br /><br />
+                Brady thrives on tackling complex problems and enjoys implementing effective solutions, supported by a solid foundation in data structures, algorithms, and database systems.
+            </h6><br />
       </Paper>
     )
 } 
