ed3de474001ce12bee740eb0651dac2a8f987900
Brady Dullanty <bdullanty@ewu.edu>
2025-03-21 21:29:38 -0700

    Updated classes taken number

--- reactfirst/src/pages/Education.js
+++ reactfirst/src/pages/Education.js
@@ -14,7 +14,7 @@ export const Education = () => {
         <h1 className="Education-title">Completed Coursework</h1>
         <br/>
         <h5 className="Education-subtitle">
-            I am currently a senior at Eastern Washington University, having completed over twenty-three courses in Computer Science and Mathematics. These classes have given me a solid foundation in software development, programming languages, algorithms, networks, and cybersecurity.
+            I am currently a senior at Eastern Washington University, having completed over twenty-6 courses in Computer Science and Mathematics. These classes have given me a solid foundation in software development, programming languages, algorithms, networks, and cybersecurity.
         </h5>
         </FadeIn>
         </div>
